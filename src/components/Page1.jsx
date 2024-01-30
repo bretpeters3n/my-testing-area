9eceee4068bad3b3aeb753cd0ac32a70330b4850
Bret Petersen <bretpetersen82@gmail.com>
2024-01-29 18:58:00 -0600

    Successfully pass an array into a URL and have that array caught on the page it goes to next

--- src/components/Page1.jsx
+++ src/components/Page1.jsx
@@ -10,23 +10,32 @@ import {
 import { useLocation, useNavigate } from "react-router-dom";
 import SearchComponent from "./SearchComponent";
 import { useParams } from "react-router";
+import stockAffirmations from "../db/stockAffirmations";
 
 const Details = () => {
+  const affirmations = stockAffirmations;
   const navigate = useNavigate();
-  const [queryParameters] = useSearchParams();
-  const params = useParams();
-  const { itemId, otherParam } = useParams();
-  console.log(itemId);
-  console.log(otherParam);
-  // const { state } = useLocation();
+
+  // const [queryParameters] = useSearchParams();
+  // const params = useParams();
+  // const { itemId, otherParam } = useParams();
+  // console.log(itemId);
+  // console.log(otherParam);
+  // const { location } = useLocation();
 
   const runSendVarToPage = () => {
     console.log("runSendVarToPage() run");
-    navigate("/page3", {
-      state: {
-        itemId: 87,
-        otherParam: "anything you want here",
-      },
+    const PAGE_PATH = `/page3`;
+    let params = affirmations;
+    console.log(params);
+    const paramString = JSON.stringify(params);
+    // const paramString = params.toString();
+    console.log(paramString);
+    const urlFormatted = `${PAGE_PATH}/query=${paramString}`;
+    console.log(urlFormatted);
+    navigate({
+      pathname: urlFormatted,
+      state: affirmations,
     });
   };
 
@@ -38,10 +47,11 @@ const Details = () => {
       <h1>Page 1</h1>
       <h2>useSearchParams</h2>
       {/* <div> */}
-      <p>Type: {queryParameters.get("type")}</p>
-      <p>Name: {queryParameters.get("name")}</p>
+      {/* <p>Type: {queryParameters.get("type")}</p> */}
+      {/* <p>Name: {queryParameters.get("name")}</p> */}
 
       <button onClick={() => runSendVarToPage()}>Page #3</button>
+      {/* <button onClick={() => console.log("test")}>Page #3</button> */}
       {/* <SearchComponent /> */}
       {/* </div> */}
     </>
